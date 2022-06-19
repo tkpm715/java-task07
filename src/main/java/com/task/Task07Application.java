6e178452b74859870646895bba14c823fa55cc37
tk <pm@letter.mail-box.ne.jp>
2022-06-19 11:43:15 +0900

    初回のコミット

---
+++ src/main/java/com/task/Task07Application.java
@@ -0,0 +1,13 @@
+package com.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task07Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/Task07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
